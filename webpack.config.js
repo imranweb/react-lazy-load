e936459cbc30adf71e8bcf3a58024567d40f1891
Mohd Imran <mohimran@WKMIN4269080.global.publicisgroupe.net>
2019-02-11 11:04:38 +0530

    Added React boilerplate with very basic config to start with

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  mode: 'development'
+};
