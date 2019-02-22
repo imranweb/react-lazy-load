1ce6abbfc8e69aab713c39afea0ab73b3fb4210f
Mohd Imran <mohimran@WKMIN4269080.global.publicisgroupe.net>
2019-02-22 12:12:09 +0530

    React Lazy Load Demo

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,54 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-class App extends Component {
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {welcome:'Hello Visitor!'};
+//   }
+
+//   render() {
+//     return(
+//       <h1>{this.state.welcome}</h1>
+//     )
+//   }
+// }
+
+// export default App;
+import React from "react";
+//import loadable from '@loadable/component';
+import StockTable from "./StockTable";
+
+//import StockChart from "./StockChart";
+
+const StockChart = React.lazy(() => import('./StockChart'))
+
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {welcome:'Hello Visitor!'};
+    this.state = {
+      selectedStock: null
+    };
   }
-
+  
   render() {
-    return(
-      <h1>{this.state.welcome}</h1>
-    )
+    const { stocks } = this.props;
+    const { selectedStock } = this.state;
+    return (
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <StockTable
+          stocks={stocks}
+          onSelect={selectedStock => this.setState({ selectedStock })}
+        />
+        {selectedStock && (
+          <StockChart
+            stock={selectedStock}
+            onClose={() => this.setState({ selectedStock: false })}
+          />
+        )}
+
+        
+      </React.Suspense>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+// ReactDOM.render(<App />, document.getElementById('main'));
+import React from "react";
+import ReactDOM from "react-dom";
+//import "./index.css";
+import App from "./components/App";
+
+function randomValue() {
+  return Math.round(1000 + Math.random() * 1000) / 10;
+}
+
+function daysAgo(days) {
+  return new Date(Date.now() - days * 24 * 60 * 60 * 1000);
+}
+
+function fakeStock(name) {
+  const today = randomValue();
+  const yesterday = randomValue();
+  const change = Math.round((1000 * (today - yesterday)) / yesterday) / 10;
+  return {
+    name,
+    today,
+    change,
+    data: [
+      { x: daysAgo(4), y: randomValue() },
+      { x: daysAgo(3), y: randomValue() },
+      { x: daysAgo(2), y: randomValue() },
+      { x: daysAgo(1), y: yesterday },
+      { x: daysAgo(0), y: today }
+    ]
+  };
+}
+
+const stocks = [
+  fakeStock("Apple"),
+  fakeStock("Citigroup"),
+  fakeStock("General Electric"),
+  fakeStock("Google"),
+  fakeStock("Microsoft")
+];
+
+ReactDOM.render(<App stocks={stocks} />, document.getElementById("main"));
